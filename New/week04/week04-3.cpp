da137e00d0076fd3d82d454ee92d9676025d3736
icexexy <zoe71515@gmail.com>
2023-03-07 12:13:48 +0800

    week04

---
+++ New/week04/week04-3.cpp
@@ -0,0 +1,34 @@
+//Week04-1.cpp step01-1
+//Step1: Input & Output OK (with while-loop)
+#include <stdio.h>
+#include <string.h>
+int testMirror(char line[50])
+{
+	return 0;
+}
+int testPalindrome(char line[50])
+{
+	int N = strlen(line);
+	int yes=0;
+	for(int i=0;i<N;i++)
+	{
+		if(line[i]==line[N-1-i]) yes++;
+	}
+	if(yes==N) return 1;
+	else return 0;
+}
+int main()
+{
+	char line[50];
+	while(scanf("%s", line)==1)
+	{
+		printf("%s --", line);
+
+		int p = testPalindrome(line);
+		int m = testMirror(line);
+		if(p==0&&m==0) printf("is not a palindrome.\n\n");
+		if(p==1&&m==0) printf("is a regular palindrom.\n\n");
+		if(p==0&&m==1) printf("is a mirror string.\n\n");
+		if(p==1&&m==1) printf("is a mirror palindrome.\n\n");
+	}
+}
---
+++ New/week04/week04-5.cpp
@@ -0,0 +1,20 @@
+//Week04-5.cpp step03-2 利用字串、字元陣列, 來把程式簡化
+//成只有一個if()下週可用這個版本, 也可用前一個36行if的版本
+#include <stdio.h>
+
+char mirror_char (char c)
+{
+    char table1[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+    char table2[]="A   3  HIL JM O   2TUVWXY51SE Z  8 ";
+    for(int i=0; table1[i]!=0;i++)
+    {
+        if(c==table1[i]) return table2[i];
+    }
+    return ' ';
+}
+int main()
+{
+    char c;
+    scanf("%c", &c);
+    printf("%c\n", mirror_char(c));
+}
